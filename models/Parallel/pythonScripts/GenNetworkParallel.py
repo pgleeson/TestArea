4ad3d3c313c2875259382897b892bb57171a1414
Padraig Gleeson <p.gleeson@gmail.com>
2011-06-16 14:42:57 +0000

    basic pyscript working

--- models/Parallel/pythonScripts/GenNetworkParallel.py
+++ models/Parallel/pythonScripts/GenNetworkParallel.py
@@ -11,20 +11,24 @@
 #
 #
 
-from sys import *
-from time import *
+import sys
+import os
 
-from java.io import File
 
-from ucl.physiol.neuroconstruct.project import ProjectManager
-from ucl.physiol.neuroconstruct.neuron import NeuronFileManager
-from ucl.physiol.neuroconstruct.nmodleditor.processes import ProcessManager
-from ucl.physiol.neuroconstruct.utils import NumberGenerator
+try:
+    from java.io import File
+except ImportError:
+    print "Note: this file should be run using ..\\..\\..\\nC.bat -python XXX.py' or '../../../nC.sh -python XXX.py'"
+    print "See http://www.neuroconstruct.org/docs/python.html for more details"
+    quit()
+
+sys.path.append(os.environ["NC_HOME"]+"/pythonNeuroML/nCUtils")
+
 from ucl.physiol.neuroconstruct.hpc.mpi import MpiSettings
-from ucl.physiol.neuroconstruct.simulation import SimulationsInfo
 
+import ncutils as nc # Many useful functions such as SimManager.runMultipleSims found here
 
-projFile = File("../Parallel.neuro.xml")
+projFile = File("../Parallel.ncx")
 
 simConfig="SmallNetwork"
 
@@ -33,77 +37,48 @@ simDuration =           500 # ms
 simDt =                 0.025 # ms
 neuroConstructSeed =    1234
 simulatorSeed =         1111
-simRefPrefix =          "SB_"
-defaultSynapticDelay =  1
-mpiConf =               MpiSettings.LEGION_4PROC
-mpiConf =               MpiSettings.MATLEM_8PROC
+simulators =            ["NEURON"]
+simConfigs = []
+simConfigs.append("Default Simulation Configuration")
+
+mpiConfig =               MpiSettings.LEGION_4PROC
+mpiConfig =               MpiSettings.MATLEM_8PROC
+mpiConfig =               MpiSettings.LOCAL_SERIAL
+
+suggestedRemoteRunTime = 2   # mins
+
+varTimestepNeuron =     False
+
+analyseSims =           True
+plotSims =              True
+plotVoltageOnly =       True
+
+simAllPrefix =          "Py_"   # Adds a prefix to simulation reference
+
+runInBackground =       mpiConfig == MpiSettings.LOCAL_SERIAL
+
+verbose =               True
+
 #######################################
 
+def testAll(argv=None):
+    if argv is None:
+        argv = sys.argv
 
-# Load neuroConstruct project
-
-print "Loading project from "+ projFile.getCanonicalPath()
-
-
-pm = ProjectManager()
-myProject = pm.loadProject(projFile)
-
-myProject.simulationParameters.setDt(simDt)
-index = 0
-
-while File( "%s/simulations/%s%i"%(myProject.getProjectMainDirectory().getCanonicalPath(), simRefPrefix,index)).exists():
-    index = index+1
-
-simRef = "%s%i"%(simRefPrefix,index)
-myProject.simulationParameters.setReference(simRef)
-
-# Use this so defaultSynapticDelay will be recorded in simulation.props and listed in SimulationBrowser GUI
-SimulationsInfo.addExtraSimProperty("defaultSynapticDelay", str(defaultSynapticDelay))
-
-simConfig = myProject.simConfigInfo.getSimConfig(simConfig)
-
-simConfig.setSimDuration(simDuration)
+    print "Loading project from "+ projFile.getCanonicalPath()
 
 
-mpiSettings = MpiSettings()
-simConfig.setMpiConf(mpiSettings.getMpiConfiguration(mpiConf))
+    simManager = nc.SimulationManager(projFile,
+                                      verbose = verbose)
 
-print "Parallel configuration: "+ str(simConfig.getMpiConf())
-
-for netConnName in simConfig.getNetConns():
-    if netConnName.count("gap")==0:
-        print "Changing synaptic delay in %s to %i"%(netConnName, defaultSynapticDelay)
-        delayGen = NumberGenerator(defaultSynapticDelay)
-        for synProps in myProject.morphNetworkConnectionsInfo.getSynapseList(netConnName):
-            synProps.setDelayGenerator(delayGen)
-
-
-pm.doGenerate(simConfig.getName(), neuroConstructSeed)
-
-while pm.isGenerating():
-        print "Waiting for the project to be generated with Simulation Configuration: "+str(simConfig)
-        sleep(1)
-
-numGenerated = myProject.generatedCellPositions.getNumberInAllCellGroups()
-
-print "Number of cells generated: " + str(numGenerated)
-
-myProject.neuronFileManager.generateTheNeuronFiles(simConfig,
-                                                    None,
-                                                    NeuronFileManager.RUN_HOC,
-                                                    simulatorSeed)
-
-print "Generated NEURON files for: "+simRef
-
-compileProcess = ProcessManager(myProject.neuronFileManager.getMainHocFile())
-
-compileSuccess = compileProcess.compileFileWithNeuron(0,0)
-
-print "Compiled NEURON files for: "+simRef
-
-if compileSuccess:
-    pm.doRunNeuron(simConfig)
-    print "Set running simulation: "+simRef
+    simManager.runMultipleSims(simConfigs =           simConfigs,
+                               simDt =                simDt,
+                               simulators =           simulators,
+                               runInBackground =      runInBackground,
+                               varTimestepNeuron =    varTimestepNeuron,
+                               mpiConfig =                mpiConfig,
+                               suggestedRemoteRunTime =   suggestedRemoteRunTime)
 
 
-exit()
+if __name__ == "__main__":
+    testAll()
