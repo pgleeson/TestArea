fbc66833856a35307305a2bf144fbda871367b73
Padraig Gleeson <p.gleeson@gmail.com>
2011-07-07 13:07:35 +0000

    Added test for equality of neuron simulations when running serial/parallel, text save/hdf5 save, hoc/PythonXML format

--- models/Parallel/pythonScripts/GenNetworkParallel.py
+++ models/Parallel/pythonScripts/GenNetworkParallel.py
@@ -32,7 +32,6 @@ import ncutils as nc # Many useful functions such as SimManager.runMultipleSims 
 
 projFile = File("../Parallel.ncx")
 
-simConfig="SmallNetwork"
 
 ###########  Main settings  ###########
 simDuration =           200 # ms
@@ -45,9 +44,13 @@ simConfigs.append("Default Simulation Configuration")
 
 
 mpiConfigs =              [MpiSettings.MATLEM_1PROC, MpiSettings.MATLEM_2PROC, MpiSettings.MATLEM_4PROC, \
-                           MpiSettings.MATLEM_8PROC, MpiSettings.MATLEM_16PROC, MpiSettings.MATLEM_32PROC, \
-                           MpiSettings.MATLEM_64PROC]#, MpiSettings.MATLEM_128PROC, MpiSettings.MATLEM_200PROC]
+                           MpiSettings.MATLEM_8PROC, MpiSettings.MATLEM_16PROC, MpiSettings.MATLEM_32PROC]#, \
+                           #MpiSettings.MATLEM_64PROC]#, MpiSettings.MATLEM_128PROC, MpiSettings.MATLEM_200PROC]
 #mpiConfigs =              [MpiSettings.LOCAL_SERIAL]
+mpiConfigs =              [MpiSettings.MATLEM_8PROC, MpiSettings.MATLEM_16PROC, MpiSettings.MATLEM_32PROC,MpiSettings.MATLEM_64PROC]
+
+multipleRuns =          [-1, -2, -3, -4]
+#multipleRuns =          [-1, -2]
 
 suggestedRemoteRunTime = 2   # mins
 
@@ -59,11 +62,15 @@ plotVoltageOnly =       True
 
 simAllPrefix =          "Py_"   # Adds a prefix to simulation reference
 
-runInBackground =       mpiConfigs == [MpiSettings.LOCAL_SERIAL]
+runInBackground =       True
 
 verbose =               True
-runSims =               False
 runSims =               True
+runSims =               False
+
+saveAsHdf5 =            True
+#saveAsHdf5 =            False
+saveOnlySpikes =        True
 
 numConcurrentSims = 4
 if mpiConfigs != [MpiSettings.LOCAL_SERIAL]: numConcurrentSims = 30
@@ -81,6 +88,7 @@ def testAll(argv=None):
                                       numConcurrentSims = numConcurrentSims,
                                       verbose = verbose)
 
+
     allSims = simManager.runMultipleSims(simConfigs =             simConfigs,
                                simDt =                   simDt,
                                simDuration =             simDuration,
@@ -90,24 +98,67 @@ def testAll(argv=None):
                                mpiConfigs =              mpiConfigs,
                                suggestedRemoteRunTime =  suggestedRemoteRunTime,
                                simRefGlobalPrefix =      simAllPrefix,
-                               runSims =                 runSims)
+                               runSims =                 runSims,
+                               maxElecLens =             multipleRuns,
+                               saveAsHdf5 =              saveAsHdf5,
+                               saveOnlySpikes =          saveOnlySpikes)
 
     while (len(simManager.allRunningSims)>0):
         print "Waiting for the following sims to finish: "+str(simManager.allRunningSims)
         time.sleep(5) # wait a while...
         simManager.updateSimsRunning()
 
+    times = []
+    procNums = []
     for sim in allSims:
         simDir = File(projFile.getParentFile(), "/simulations/"+sim)
-
         try:
             simData = SimulationData(simDir)
             simData.initialise()
             simTime = simData.getSimulationProperties().getProperty("RealSimulationTime")
             print "Simulation: %s took %s seconds"%(sim, simTime)
+            times.append(float(simTime))
+            paraConfig = simData.getSimulationProperties().getProperty("Parallel configuration")
+            print paraConfig
+            numProc = int(paraConfig[max(paraConfig.find(" host, ")+7, paraConfig.find(" hosts, ")+8):paraConfig.find(" processor")])
+            procNums.append(numProc)
 
         except:
             print "Error analysing simulation data from: %s"%simDir.getCanonicalPath()
+            print sys.exc_info()
+
+
+
+
+    print times
+    print procNums
+
+    '''
+    import matplotlib.pyplot as plt
+    lines = plt.loglog(times, procNums, 'r:')
+
+    plt.ylabel('Simulation time')
+    plt.xlabel('Number of processors')
+    plt.show()'''
+
+    from ucl.physiol.neuroconstruct.gui.plotter import PlotManager
+    from ucl.physiol.neuroconstruct.gui.plotter import PlotCanvas
+
+    from ucl.physiol.neuroconstruct.dataset import DataSet
+
+    plotFrame = PlotManager.getPlotterFrame("Time for simulation run on different numbers of processors", 0, 1)
+
+    plotFrame.setViewMode(PlotCanvas.INCLUDE_ORIGIN_VIEW)
+
+    info = "Simulation times for: "+str(procNums)
+
+    dataSet = DataSet(info, info, "#", "s", "Number of processors", "Simulation time")
+    dataSet.setGraphFormat(PlotCanvas.USE_CIRCLES_FOR_PLOT)
+
+    for i in range(len(times)):
+        dataSet.addPoint(procNums[i],times[i]*procNums[i])
+
+    plotFrame.addDataSet(dataSet)
 
 if __name__ == "__main__":
     testAll()
