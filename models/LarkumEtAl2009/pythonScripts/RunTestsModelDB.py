364d0a6127479f258ea95979170101f4a119f188
Padraig Gleeson <p.gleeson@gmail.com>
2012-04-24 15:07:15 +0000

    Added preliminary python scripts to test spiketimes of default sim config & comparison to modeldb version

---
+++ models/LarkumEtAl2009/pythonScripts/RunTestsModelDB.py
@@ -0,0 +1,94 @@
+#
+#
+#   File to test current configuration of CA1Pyramidal cell project. 
+#
+#   To execute this type of file, type '..\..\..\nC.bat -python XXX.py' (Windows)
+#   or '../../../nC.sh -python XXX.py' (Linux/Mac). Note: you may have to update the
+#   NC_HOME and NC_MAX_MEMORY variables in nC.bat/nC.sh
+#
+#   Author: Padraig Gleeson
+#
+#   This file has been developed as part of the neuroConstruct project
+#   This work has been funded by the Medical Research Council and the
+#   Wellcome Trust
+#
+#
+
+import sys
+import os
+
+try:
+    from java.io import File
+except ImportError:
+    print "Note: this file should be run using ..\\..\\..\\nC.bat -python XXX.py' or '../../../nC.sh -python XXX.py'"
+    print "See http://www.neuroconstruct.org/docs/python.html for more details"
+    quit()
+
+sys.path.append(os.environ["NC_HOME"]+"/pythonNeuroML/nCUtils")
+
+import ncutils as nc # Many useful functions such as SimManager.runMultipleSims found here
+
+projFile = File(os.getcwd(), "../LarkumEtAl2009.ncx")
+
+##############  Main settings  ##################
+
+simConfigs = []
+
+simConfigs.append("test_IClamp")
+#simConfigs.append("CA1Cell")
+
+simDt =                 0.025
+
+simulators =            ["NEURON"]
+
+numConcurrentSims =     1
+
+varTimestepNeuron =     False
+
+plotSims =              True
+plotVoltageOnly =       True
+runInBackground =       True
+analyseSims =           True
+
+verbose = True
+
+#############################################
+
+
+def testAll(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    print "Loading project from "+ projFile.getCanonicalPath()
+
+
+    simManager = nc.SimulationManager(projFile,
+                                      numConcurrentSims = numConcurrentSims,
+                                      verbose = verbose)
+
+    simManager.runMultipleSims(simConfigs =           simConfigs,
+                               simDt =                simDt,
+                               simulators =           simulators,
+                               runInBackground =      runInBackground,
+                               varTimestepNeuron =    varTimestepNeuron)
+
+    simManager.reloadSims(plotVoltageOnly =   plotVoltageOnly,
+                          plotSims =          plotSims,
+                          analyseSims =       analyseSims)
+
+    # These were discovered using analyseSims = True above.
+    # They need to hold for all simulators
+    spikeTimesToCheck = {'pyr_group_0': [61.925,71.45,85.05,132.3,142.9,192.05,202.675,250.775,261.5,308.5,319.275,365.25,376.125,421.15,432.075]}
+
+    spikeTimeAccuracy = 6 #TODO: Make this smaller!!
+
+    report = simManager.checkSims(spikeTimesToCheck = spikeTimesToCheck,
+                                  spikeTimeAccuracy = spikeTimeAccuracy)
+
+    print report
+
+    return report
+
+
+if __name__ == "__main__":
+    testAll()
